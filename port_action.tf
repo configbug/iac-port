683578a8ad6761723c6dd6bb93aba0ae0151b09c
Daniel Huaman <danielhuaman@outlook.com>
2025-02-24 00:00:12 -0500

    feat: new provider version

---
+++ port_action.tf
@@ -0,0 +1,38 @@
+resource "port_action" "demo_import_action" {
+  title       = "Prueba"
+  identifier  = "prueba"
+  publish     = true
+  description = "Probando"
+  github_method = {
+    org                    = "configbug"
+    repo                   = "port-actions"
+    report_workflow_status = "true"
+    workflow               = "action-port.yml"
+    workflow_inputs = jsonencode(
+      {
+        organization = "{{ .organization }}"
+        port_context = {
+          runId = "{{ .run.id }}"
+        }
+        "{{ spreadValue() }}" = "{{ .inputs }}"
+      }
+    )
+  }
+  required_approval = "false"
+  self_service_trigger = {
+    operation = "CREATE"
+    user_properties = {
+      number_props = {
+        "aviso" = {
+          description = "Numero de avisos"
+          title       = "Aviso"
+        },
+      }
+    }
+  }
+}
+
+resource "port_action" "demo_import_action_2" {
+  title       = "Prueba"
+  identifier  = "prueba_2"
+}
